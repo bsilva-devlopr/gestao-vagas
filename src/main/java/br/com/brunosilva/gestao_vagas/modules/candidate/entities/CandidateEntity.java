b776b50d0a467632525cf4e2b689e8bd0c9cffd9
BrunoMoreira <bruno.moreira@lecom.com.br>
2025-08-30 12:45:45 -0300

    atualizando os imports

--- src/main/java/br/com/brunosilva/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ src/main/java/br/com/brunosilva/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
