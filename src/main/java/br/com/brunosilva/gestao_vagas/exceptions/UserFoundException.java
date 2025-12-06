7b92d477c2ead1eed500c38ea859d6eca6a7ca2e
BrunoMoreira <bruno.moreira@lecom.com.br>
2025-12-06 12:27:54 -0300

    Atualizando

--- src/main/java/br/com/brunosilva/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/br/com/brunosilva/gestao_vagas/exceptions/UserFoundException.java
@@ -2,6 +2,6 @@ package br.com.brunosilva.gestao_vagas.exceptions;
 
 public class UserFoundException extends RuntimeException {
     public UserFoundException() {
-        super(message);
+        super();
     }
 }
